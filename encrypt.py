f87acf9d620e2abb12f0b1c88088546f47f45352
block <maglevdude1@outlook.com>
2024-09-15 16:45:02 -0800

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,10 @@
+key = ' abcdefghijklmnopqrstuvwxyz'  # 'key' string maps to input chars in order
+while True:
+    e = input('Enter a sequence of letters: ').lower()  # prompt for input
+    liste=[]
+    for char in e:  # iterate over input
+        try:
+            liste.append(key.index(char)**2)  # print the numerical index of 'char' in 'key
+        except ValueError:  # input character not in 'key' (e.g. '!')
+            pass  # do nothing
+    print(*liste)
