7ae5d1f805dc6ff66f175e0aa25031372dab75a8
block <maglevdude1@outlook.com>
2024-09-25 15:51:19 -0800

    added more encryption
    I added a decimal exponent factor to the encryption

--- encrypt.py
+++ encrypt.py
@@ -4,7 +4,7 @@ while True:
     liste=[]
     for char in e:  # iterate over input
         try:
-            liste.append(key.index(char)**2)  # print the numerical index of 'char' in 'key
+            liste.append(key.index((char)**2)**0.153)  # print the numerical index of 'char' in 'key
         except ValueError:  # input character not in 'key' (e.g. '!')
             pass  # do nothing
     print(*liste)
